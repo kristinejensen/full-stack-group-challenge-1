a0749489cdebd49891b92bee558c1142c1f39d21
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-09 13:52:37 -0600

    Client.js and Angular Routes defined

---
+++ public/scripts/client.js
@@ -0,0 +1,28 @@
+var myApp = angular.module('WarehouseApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+
+
+    .when('/warehouse', {
+      templateUrl: 'views/warehouse-view',
+      controller: 'warehouseController',
+      controllerAs: 'wc'
+    })
+
+    .when('/customers' {
+      templateUrl: 'views/customer-view.html',
+      controller: 'customersController',
+      controllerAs: 'cc'
+    })
+
+    .when('/orders' {
+      templateUrl: 'views/orders-view.html',
+      controller: 'ordersController',
+      controllerAs: 'oc'
+    })
+
+    .otherwise({
+      redirectTo: 'warehouse'
+    })
+}]);
