5e349b484c82eca140488a80e6893b165aea3d1a
Kristine Jensen <kristine.b.jensen@gmail.com>
2017-03-09 13:26:58 -0600

    Server and static html working

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var router = require('./routes/routes.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, './public')));
+//
+// app.use('/colors', colorRouter);
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), function() {
+    console.log('Listening on port: ', app.get('port'));
+});
